2cb2c21667cebc67cfe8b1f03d43750382a11bcf
Paula Bagnati <pau.bagnati@gmail.com>
2022-12-11 09:55:26 -0300

    js

--- script.js
+++ script.js
@@ -1,40 +1,99 @@
 alert ("Bienvenidos a DINERO FACIL. ¡Cotiza tu préstamo YA!");
+alert ("Vamos a solicitar unos datos para continuar");
 
-let prestamoSolicitado = [];
+//capturando datos de la persona
+let nombre = prompt("Ingresa tu nombre").toLocaleLowerCase();
+let age = 0;
+let email = ``;
 
-function solicitarPrestamo (){
-    let banco = prompt(`Selecciona tu banco: 
-    1. Galicia
-    2. Santander
-    3. Frances
-    4. Provincia
-    5. Nación
-    6. Superville
-    7. HSBC
-    8. ICBC
-    9. otros`);    
+if (nombre == `` ){
+    console.log("Ingresa tu nombre por favor");
+}else{
+    age = prompt("Ingresa tu edad").toLocaleLowerCase();
+    if(age == ``){
+        console.log ("esta vacio")
+        prompt = Number (("Ingrese su edad, por favor"));
+    }
 }
 
-let email = prompt("Ingresa tu email")
 
-let eligeBanco = prompt(`Selecciona tu banco: 
-1. Galicia
-2. Santander
-3. Frances
-4. Provincia
-5. Nación
-6. Superville
-7. HSBC
-8. ICBC
-9. otros`);
+//creando objeto persona
+const persona = {
+    nombre : nombre,
+    edad : age,
+    email :email,
+}
 
-let bancos = [];
-let cuotas = [3, 6, 12, 24]
-
-// function prestamoSolicitado {
+if (persona.edad <= 17){
+    alert("Usted es menos de edad");
+}else{
+    email = prompt("Ingresa tu email").toLocaleLowerCase();
+}
 
 
+
+
+
+
+
+
+// const banco = prompt(`Selecciona tu banco: 
+// 1. Galicia
+// 2. Santander
+// 3. BVBA
+// 4. Provincia
+// 5. Nación
+// 6. Superville
+// 7. HSBC
+// 8. ICBC
+// 9. otros`).toLocaleLowerCase();  
+
+// const cuotas =Number( prompt(`seleciona cuotas :
+// 3 - 6 - 12 -24`));
+
+// let dinero = Number(prompt("¿Cuanto dinero deseas solicitar?"));
+
+
+// const PrestamoSolicitado = {
+//     banco : banco,
+//     edad : edad,
+//     email :email,
 // }
+
+
+
+
+// function solicitarPrestamo (){
+//     let banco = prompt(`Selecciona tu banco: 
+//     1. Galicia
+//     2. Santander
+//     3. BVBA
+//     4. Provincia
+//     5. Nación
+//     6. Superville
+//     7. HSBC
+//     8. ICBC
+//     9. otros`).toLocaleLowerCase();  
+    
+//     if (banco === Galicia || banco == Santander ||banco == BVBA ||banco == Provincia ||banco ==Nación ||banco == Superville ||banco == HSBC ||banco == ICBC ||banco == otros){
+//         bancos.push(prestamoSolicitado);
+//         console.log(prestamoSolicitado);
+
+//     }else{
+//         prompt("Selecciona una opcion de la lista");
+//     }
+    
+// }
+
+// solicitarPrestamo();
+
+
+
+
+
+
+
+
 
 
 
