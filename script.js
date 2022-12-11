a73c9a375ac901ccfa9c01df9abedb77d557fcff
Paula Bagnati <pau.bagnati@gmail.com>
2022-12-11 07:26:49 -0300

    iniciando con js

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
 alert ("Bienvenidos a DINERO FACIL. ¡Cotiza tu préstamo YA!");
 
+let prestamoSolicitado = [];
+
+function solicitarPrestamo (){
+    let banco = prompt(`Selecciona tu banco: 
+    1. Galicia
+    2. Santander
+    3. Frances
+    4. Provincia
+    5. Nación
+    6. Superville
+    7. HSBC
+    8. ICBC
+    9. otros`);    
+}
+
+let email = prompt("Ingresa tu email")
+
+let eligeBanco = prompt(`Selecciona tu banco: 
+1. Galicia
+2. Santander
+3. Frances
+4. Provincia
+5. Nación
+6. Superville
+7. HSBC
+8. ICBC
+9. otros`);
+
+let bancos = [];
+let cuotas = [3, 6, 12, 24]
+
+// function prestamoSolicitado {
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
